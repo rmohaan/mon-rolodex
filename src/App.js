7f9cf8df49cf4f4d61411cce5cc7c1febbcaaef0
Mohaan Raja <mohaan.raja@freshworks.com>
2024-02-07 09:42:37 +0530

    Added API fetch and search-filtering capability

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
+import { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor() {
+    super()
+    this.state = {
+      search: "",
+      masters: [],
+      filtered: []
+    }
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.org/users/")
+    .then((res) => res.json())
+    .then((res) => {
+      this.setState({
+        masters: res,
+        filtered: res
+      })
+    })
+  }
+
+  changeName = () => {
+    this.setState({
+      name: 'Raja'
+    })
+  }
+
+  handleChange = (e) => {
+    e.preventDefault()
+
+    const newSearchKey = e.target.value.toLowerCase()
+
+    const filtered = this.state.masters.filter((master) => {
+      return master.firstname.toLowerCase().includes(newSearchKey) || master.lastname.toLowerCase().includes(newSearchKey) || master.email.toLowerCase().includes(newSearchKey)
+    })
+
+    console.log(filtered)
+
+    this.setState({
+      search: e.target.value,
+      filtered: filtered
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+      <div>
+        <input 
+          type="text" 
+          value={this.state.search} 
+          placeholder='Search' 
+          onChange={this.handleChange} />
+          
+      </div>
+       {
+        this.state.filtered.map((master, i) => {
+          return (<div key={i}> {master.firstname} {master.lastname} </div>)
+        })
+      }
+      </div>
+    );
+  }
 }
 
 export default App;
